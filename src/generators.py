2c1aef4b855802cd98ffb847eccc2ea035bea510
Сергей Алтухов <uncleserezha1@mail.ru>
2025-07-12 13:21:21 +0300

    error correction

--- src/generators.py
+++ src/generators.py
@@ -1,18 +1,24 @@
 from typing import Union
 
 
-def filter_by_currency(transactions: list[dict], currency: Union[str]) -> list[dict]:
+def filter_by_currency(transactions: list[dict], currency: Union[str]):
     """Функция, которая принимает на вход список словарей, представляющих транзакции.
     Функция должна возвращать итератор,который поочередно выдает транзакции,
     где валюта операции соответствует заданной (например, USD)."""
     result = [x for x in transactions if x["operationAmount"]["currency"]["code"] == currency]
-    return result
+    x = 0
+    while True:
+        yield result[x]
+        x += 1
 
 
-def transaction_descriptions(transactions: list[dict]):
+def transaction_descriptions(transactions: list[dict]) :
     """Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
     result = [x["description"] for x in transactions]
-    yield "\n".join(result)
+    x = 0
+    while True:
+        yield result[x]
+        x += 1
 
 
 def card_number_generator(start: Union[int], stop: Union[int]) -> Union[str]:
